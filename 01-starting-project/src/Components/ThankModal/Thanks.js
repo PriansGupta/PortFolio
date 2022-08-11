1abb70fe777eccd73cdd6d12717d91d876cfd41a
Priyansh <priyanshg615@gmail.com>
2022-08-11 14:01:58 +0530

    Complete

--- 01-starting-project/src/Components/ThankModal/Thanks.js
+++ 01-starting-project/src/Components/ThankModal/Thanks.js
@@ -1,9 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Fragment } from "react/cjs/react.production.min";
+import ListGroup from "react-bootstrap/ListGroup";
 import "./Thanks.css";
 
 const Thanks = (props) => {
+  const is = props.text === "Projects will be listed Soon.";
+  console.log(is);
+  let ele;
+  if (is) {
+    ele = (
+      <ListGroup as="ol" numbered>
+        <ListGroup.Item as="li">
+          <a
+            style={{ textDecoration: "none" ,color:"black"}}
+            href="https://weather-app-prians.herokuapp.com/"
+          >
+            Weather App
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item as="li">
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href="https://beezpay-bcd34.web.app/"
+          >
+            Beez-Pay
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item as="li">
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href="https://priansgupta.github.io/CRUD-App/"
+          >
+            CRUD-App
+          </a>
+        </ListGroup.Item>
+        <ListGroup.Item as="li">
+          <a
+            style={{ textDecoration: "none", color: "black" }}
+            href="https://github.com/PriansGupta/Babble"
+          >
+            Babble-ChatApp
+          </a>
+        </ListGroup.Item>
+      </ListGroup>
+    );
+  } else {
+    ele = props.text;
+  }
   const Backdrop = (props) => {
     return (
       <div
@@ -28,15 +72,17 @@ const Thanks = (props) => {
           <span className="close_bt" onClick={props.onClose}>
             <ion-icon name="close"></ion-icon>
           </span>
-          <div
-            className="Indi_thnx"
-            style={{ border: `solid 3.5px ${props.color}` }}
-          >
-            <span className="check_icon" style={{ color: `${props.color}` }}>
-              <ion-icon name={props.icon}></ion-icon>
-            </span>
-          </div>
-          <div className="Msg">{props.text}</div>
+          {!is && (
+            <div
+              className="Indi_thnx"
+              style={{ border: `solid 3.5px ${props.color}` }}
+            >
+              <span className="check_icon" style={{ color: `${props.color}` }}>
+                <ion-icon name={props.icon}></ion-icon>
+              </span>
+            </div>
+          )}
+          <div className="Msg">{ele}</div>
         </div>
       </Fragment>
     );
